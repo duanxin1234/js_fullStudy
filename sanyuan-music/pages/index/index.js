2c63b73fcb115966e883c130433639f8e26ae681
duanxin1234 <863351709@qq.com>
2020-07-29 20:19:03 +0800

    豆瓣小程序

---
+++ sanyuan-music/pages/index/index.js
@@ -0,0 +1,54 @@
+//index.js
+//获取应用实例
+const app = getApp()
+
+Page({
+  data: {
+    bannerList: [],
+    recommendList: [],
+    isRecommendListLoading: true
+  }
+},
+  //事件处理函数
+  bindViewTap: function () {
+    wx.navigateTo({
+      url: '../logs/logs'
+    })
+  },
+  onLoad() {
+  wx.request({
+    url: 'http://47.98.159.95/m-api/banner',
+    success(res) {
+
+
+      this.setData({
+        bannerList: res.data.banners,
+      })
+    }
+  })
+    wx.request({
+    url: 'http://47.98.159.95/m-api/personalized',
+    success(res) {
+
+
+      this.setData({
+        recommendList: res.data.banners,
+        isRecommendListLoading: false,
+      }
+          
+        })
+}
+    })
+
+
+
+  },
+getUserInfo: function (e) {
+  console.log(e)
+  app.globalData.userInfo = e.detail.userInfo
+  this.setData({
+    userInfo: e.detail.userInfo,
+    hasUserInfo: true
+  })
+}
+})
---
+++ spa/readme.md
@@ -0,0 +1,24 @@
+就业公式=github+leetcode+掘金写文章+商业项目+源码分析+直播+读经典原著
+创造力+代码能力（刻意联系）* 影响力（掘金 ）
+http://47.98.159.95:8010/#/recommend
+1，图解http protocol 协议
+2 ip 域名 阿里云服务器
+#/recommend 访问路径 锚链接
+http://47.98.159.95:8010/ 首页
+
+1.目录的概念
+ /pages
+ 目录架构
+ 网页架构
+ 作弊功能
+
+ 2，方法
+ -移动网站用来借鉴html css
+  html-》wxml 理解过程
+-小程序组件概念  练习
+recommend 页面
+需要什么把他写出来？ 数据
+html 
+动态{{}}
+指令 wx：if wx：for
+js 数据放在data setdata 
