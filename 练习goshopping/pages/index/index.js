61b48d53ee172e7dbda810eb362a94a9f7c6b40e
duanxin1234 <863351709@qq.com>
2020-08-16 13:34:49 +0800

    练习

---
+++ 练习goshopping/pages/index/index.js
@@ -0,0 +1,26 @@
+//index.js
+//获取应用实例
+const app = getApp()
+
+Page({
+  data: {
+    items: [
+      {
+        num: 1200,
+        isstar: true
+      },
+      {
+        num: 800,
+        isstar: false
+      },
+    ]
+  },
+  //事件处理函数
+
+  onLoad: function () {
+
+  },
+  getUserInfo: function (e) {
+
+  }
+})
