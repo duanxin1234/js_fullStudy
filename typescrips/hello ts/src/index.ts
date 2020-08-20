3745a8f3d837b27fe1e0c00bc1f7e116e38f094b
duanxin1234 <863351709@qq.com>
2020-08-20 12:05:17 +0800

    ts

---
+++ typescrips/hello ts/src/index.ts
@@ -0,0 +1,32 @@
+// function greeter(person: string) {
+//     return "hello" + person
+// }
+// greeter("1")
+// const user = "wn"
+
+// //类型
+// let isLoading: boolean = false
+
+
+
+// let num: string = "a"
+// num = "dd"
+
+// //空值
+// function wad() {
+//     return 1
+// }
+// let a: undefined = undefined
+
+// //Symblo 唯一
+// let syml = Symbol("key1")
+// let sym2 = Symbol("key1")
+
+
+// BigInt 
+const max = BigInt(Number.MAX_SAFE_INTEGER)
+const max1 = max + max + max
+const max2 = max + max
+console.log(max1 === max2);
+
+
