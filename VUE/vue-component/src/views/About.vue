05f9431035019452ef34fa4df40a0a76d333d408
duanxin1234 <863351709@qq.com>
2020-08-27 12:04:52 +0800

    vue-compont

---
+++ VUE/vue-component/src/views/About.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="about">
+    <user :userName="name" @nameChange="updata"></user>
+    <person :babyname="catname"></person>
+  </div>
+</template>
+<script>
+// @ is an alias to /src
+import user from "@/components/user";
+import person from "@/components/person";
+export default {
+  data() {
+    return {
+      name: "pp",
+      catname: undefined,
+    };
+  },
+  components: {
+    user,
+    person,
+  },
+  methods: {
+    updata(catname) {
+      this.catname = catname;
+    },
+  },
+};
+</script>
+<style>
+.about {
+  color: pink;
+}
+</style>
---
+++ VUE/vue-component/src/views/Home.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="home">
+    <p>{{title}}</p>
+    <user :userName="name" @titleChange="updata"></user>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import user from "@/components/user";
+
+export default {
+  name: "Home",
+  data() {
+    return {
+      name: "jj",
+      title: "",
+    };
+  },
+  components: {
+    user,
+  },
+  methods: {
+    updata(title) {
+      // console.log("okkk");
+      this.title = title;
+    },
+  },
+};
+</script>
