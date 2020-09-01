9d632dba9ed8e65a156d8f87a8006c3562918d80
duanxin1234 <863351709@qq.com>
2020-09-01 12:57:56 +0800

    饿了吗

---
+++ VUE/vue-elm/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+    <!-- header组件 -->
+    <v-header></v-header>
+    <router-view />
+  </div>
+</template>
+
+<script>
+import VHeader from '@/components/v-header/v-header.vue';
+
+export default {
+  components: {
+    VHeader,
+  },
+};
+</script>
+
+<style lang="stylus" scoped></style>
---
+++ VUE/vue-elm/src/components/v-header/v-header.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="header">
+    <div class="content-wrapper">
+      <div class="avatar">
+        <img
+          width="64"
+          height="64"
+          src="https://ss0.bdstatic.com/70cFvHSh_Q1YnxGkpoWK1HF6hhy/it/u=246748765,3065771981&fm=26&gp=0.jpg"
+          alt
+        />
+      </div>
+      <div class="content">
+        <div class="title">
+          <span class="brand"></span>
+          <span class="name">好吃店</span>
+        </div>
+        <div class="description">蜂鸟专送/38分送达</div>
+        <div class="support">
+          <img src="./brand@2x.png" alt />
+          <span class="text">在线支付满28减5</span>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
