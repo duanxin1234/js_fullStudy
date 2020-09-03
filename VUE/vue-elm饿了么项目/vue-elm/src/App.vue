7073a9e32c498bb1c3e1fcf27f9273563b03201f
duanxin1234 <863351709@qq.com>
2020-09-03 12:24:33 +0800

    vue项目

--- VUE/vue-elm饿了么项目/vue-elm/src/App.vue
+++ VUE/vue-elm饿了么项目/vue-elm/src/App.vue
@@ -2,20 +2,31 @@
   <div id="app">
     <v-header :seller="seller"></v-header>
     <!-- header 组件 -->
-    <router-view/>
+    <div class="tab">
+        <div class="tab-wrapper">
+          <router-link to="/">aa</router-link>
+        </div>
+        <div class="tab-wrapper">
+          <router-link to="/comment">bb</router-link>
+        </div>
+        <div class="tab-wrapper">
+          <router-link to="/seller">cc</router-link>
+        </div>
+    </div>
+    <router-view :data="seller" />
   </div>
 </template>
 
 <script>
-import VHeader from '@/components/v-header/v-header.vue';
-import { getSeller } from '@/api';
-import qs from 'query-string'
+import VHeader from "@/components/v-header/v-header.vue";
+import { getSeller } from "@/api";
+import qs from "query-string";
 
 export default {
   data() {
     return {
       seller: {
-        id: qs.parse(location.search).id
+        id: qs.parse(location.search).id,
       },
     };
   },
@@ -27,7 +38,7 @@ export default {
       getSeller({
         id: this.seller.id,
       }).then((seller) => {
-        this.seller = Object.assign({}, this.seller, seller)
+        this.seller = Object.assign({}, this.seller, seller);
         // console.log(this.seller);
       });
     },
@@ -39,5 +50,22 @@ export default {
 </script>>
 
 <style lang="stylus" scoped>
+.tab
+  width 100%
+  height 50px
+  line-height 50px
+  display flex
+  .tab-wrapper
+    flex 1
+    text-align center
+    color #666666
+    a
+      display block
+      width 100%
+    .router-link-exact-active
+      color red
+      border-bottom 1px solid red
 
+
+    
 </style>
