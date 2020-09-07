50f30e0475efc9687c8b2786dabc56d652033b9d
duanxin1234 <863351709@qq.com>
2020-09-07 17:25:06 +0800

    vuex

---
+++ VUE/vue-x/src/components/homeChlid.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <P>页面的子组件</P>
+    <hr />
+    <h3>{{count}}</h3>
+    <div class="btn">
+      <button @click="add">+</button>
+      <br />
+      <button @click="reduce">-</button>
+    </div>
+  </div>
+</template>
+
+<script>
+import store from "@/vuex/store.js";
+import { mapState, mapMutations } from "vuex";
+export default {
+  data() {
+    return {
+      msg: "hello world!",
+    };
+  },
+  store,
+  // computed: mapState({
+  //   count:state=>state.count
+  // }),
+  computed: mapState(["count"]),
+  // methods: {
+  //   add() {
+  //     this.$store.commit("add");
+  //   },
+  //   reduce() {
+  //     this.$store.commit("reduce");
+  //   },
+  // },
+  methods: mapMutations(["add", "reduce"]),
+};
+</script>
+
+<style>
+</style>
