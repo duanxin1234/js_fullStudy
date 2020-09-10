dc61e3ac3038ebe946d0566fc7840eefa1b4d043
duanxin1234 <863351709@qq.com>
2020-09-10 23:15:55 +0800

    'vue'

---
+++ VUE/v-music项目/vue-music(2)/src/App.vue
@@ -0,0 +1,62 @@
+<template>
+  <div id="app">
+    <v-header>
+      <i class="iconfont" slot="left-icon">&#xe96d;</i>
+      <span slot="content">我的音乐</span>
+      <i class="iconfont" slot="right-icon">&#xe911;</i>
+    </v-header>
+    <v-tab></v-tab>
+
+    <keep-alive>
+      <router-view/>
+    </keep-alive>
+
+    <!-- menu -->
+    <v-sidebar></v-sidebar>
+  </div>
+</template>
+
+<script>
+import header from '@/components/header'
+import sidebar from '@/components/sidebar'
+import tab from '@/components/tab'
+
+export default {
+  components: {
+    'v-header': header,
+    'v-sidebar': sidebar,
+    'v-tab': tab
+  }
+}
+</script>
+
+<style lang="stylus">
+@font-face {
+  font-family: 'iconfont';  /* project id 2052911 */
+  src: url('//at.alicdn.com/t/font_2052911_tiyyyi50ntt.eot');
+  src: url('//at.alicdn.com/t/font_2052911_tiyyyi50ntt.eot?#iefix') format('embedded-opentype'),
+  url('//at.alicdn.com/t/font_2052911_tiyyyi50ntt.woff2') format('woff2'),
+  url('//at.alicdn.com/t/font_2052911_tiyyyi50ntt.woff') format('woff'),
+  url('//at.alicdn.com/t/font_2052911_tiyyyi50ntt.ttf') format('truetype'),
+  url('//at.alicdn.com/t/font_2052911_tiyyyi50ntt.svg#iconfont') format('svg');
+}
+.iconfont{
+  font-family:"iconfont" !important;
+  font-size:16px;font-style:normal;
+  -webkit-font-smoothing: antialiased;
+  -webkit-text-stroke-width: 0.2px;
+  -moz-osx-font-smoothing: grayscale;
+}
+html,
+body {
+  line-height: 1;
+  font-family: PingFang SC, STHeitiSC-Light, Helvetica-Light, arial, sans-serif;
+  -webkit-user-select: none;
+  -moz-user-select: none;
+  -ms-user-select: none;
+  user-select: none;
+  -webkit-tap-highlight-color: transparent;
+  background: rgba(8, 5, 58, 0.9);
+  color: #fff;
+}
+</style>
