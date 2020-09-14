0aaecc66adc60e0ec3e70e2fcbc0e863638646a0
duanxin1234 <863351709@qq.com>
2020-09-14 21:56:28 +0800

    koa

---
+++ VUE/vue-nodebook/note-book/controllers/defaultConfig.js
@@ -0,0 +1,11 @@
+const config = {
+    database: {
+        DATABASE: "test",
+        USERNMAE: 'root',
+        PASSWORD: '111',
+        PORT: '3306',
+        HOST:'localhost'
+    }
+}
+
+module.exports=config
